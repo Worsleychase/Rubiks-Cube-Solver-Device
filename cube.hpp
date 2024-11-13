742ab137ac44ea6603b455c595f6dd16b41641cc
Worsleychase <68138593+Worsleychase@users.noreply.github.com>
2024-11-12 21:11:25 -0700

    Add files via upload

--- cube.hpp
+++ cube.hpp
@@ -19,7 +19,6 @@ public:
 
     int rotationCount;
 
-
     virCube() {
         rotationCount = 0;
         for (int layer = 0; layer < 3; layer++) {
@@ -72,7 +71,7 @@ public:
         for (int i = 0; i < 3; i++) {
             // Left face
             for (int j = 0; j < 3; j++) {
-                std::cout << pieces[j][i * 3].col[0] << " ";
+                std::cout << pieces[2-j][i * 3].col[0] << " ";
             }
             std::cout << " ";
 
@@ -89,7 +88,7 @@ public:
             std::cout << " ";
 
             // Back face
-            for (int j = 0; j < 3; j++) {
+            for (int j = 2; j >= 0; j--) {
                 std::cout << pieces[2][i * 3 + j].col[2] << " ";
             }
             std::cout << std::endl;
@@ -120,30 +119,268 @@ public:
         piece temp[9];
         switch (face)
         {
-        case 'f':
+        case 'f': {
             for (int i = 0; i < 9; i++) {
                 temp[i] = pieces[0][i];
             }
 
             for (int i = 0; i < 9; i++) {
-                pieces[0][i].rotate(rotMatricies.FCW);
+                temp[i].rotate(rotMatricies.FCW);
             }
-            break;
 
             for (int i = 0; i < 9; i++) {
                 int x = temp[i].pos[0];
                 int y = temp[i].pos[1];
                 int newIndex = (1 - y) * 3 + (x + 1);
                 pieces[0][newIndex] = temp[i];
-            }            
-
-        case 'F':
-            for (int i = 0; i < 9; i++) {
-                pieces[0][i].rotate(rotMatricies.FCCW);
             }
             break;
-        } 
+            }
+
+        case 'F': {
+            for (int i = 0; i < 9; i++) {
+                temp[i] = pieces[0][i];
+            }
+
+            for (int i = 0; i < 9; i++) {
+                temp[i].rotate(rotMatricies.FCCW);
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int x = temp[i].pos[0];
+                int y = temp[i].pos[1];
+                int newIndex = (1 - y) * 3 + (x + 1);
+                pieces[0][newIndex] = temp[i];
+            }
+            break;
+            }
+
+        case 'r': {
+            int index = 0;
+            for (int layer = 0; layer < 3; layer++) {
+                for (int pos = 2; pos < 9; pos += 3) {
+                    temp[index++] = pieces[layer][pos];
+                }
+            }
+
+            for (int i = 0; i < 9; i++) {
+                temp[i].rotate(rotMatricies.RCW);
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int y = temp[i].pos[1];
+                int z = temp[i].pos[2];
+                int layer = 1 - z;
+                int pos = (1 - y) * 3 + 2;
+                pieces[layer][pos] = temp[i];
+            }
+            break;            
+            }
+
+        case 'R': {
+            int index = 0;
+            for (int layer = 0; layer < 3; layer++) {
+                for (int pos = 2; pos < 9; pos += 3) {
+                    temp[index++] = pieces[layer][pos];
+                }
+            }
+
+            for (int i = 0; i < 9; i++) {
+                temp[i].rotate(rotMatricies.RCCW);
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int y = temp[i].pos[1];
+                int z = temp[i].pos[2];
+                int layer = 1 - z;
+                int pos = (1 - y) * 3 + 2;
+                pieces[layer][pos] = temp[i];
+            }
+            break;
+        }
+
+        case 'b': {
+            int index = 0;
+            for (int i = 0; i < 9; i++) {
+                temp[i] = pieces[2][i];
+            }
+
+            for (int i = 0; i < 9; i++) {
+                temp[i].rotate(rotMatricies.BCW);
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int x = temp[i].pos[0];
+                int y = temp[i].pos[1];
+                int newIndex = (1 - y) * 3 + (x + 1);
+                pieces[2][newIndex] = temp[i];
+            }
+            break;
+        }
+
+        case 'B': {
+            int index = 0;
+            for (int i = 0; i < 9; i++) {
+                temp[i] = pieces[2][i];
+            }
+
+            for (int i = 0; i < 9; i++) {
+                temp[i].rotate(rotMatricies.BCCW);
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int x = temp[i].pos[0];
+                int y = temp[i].pos[1];
+                int newIndex = (1 - y) * 3 + (x + 1);
+                pieces[2][newIndex] = temp[i];
+            }
+            break;
+        }
+
+        case 'l': {
+            int index = 0;
+            for (int layer = 0; layer < 3; layer++) {
+                for (int pos = 0; pos < 9; pos += 3) {
+                    temp[index++] = pieces[layer][pos];
+                }
+            }
+
+            for (int i = 0; i < 9; i++) {
+                temp[i].rotate(rotMatricies.LCW);
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int y = temp[i].pos[1];
+                int z = temp[i].pos[2];
+                int layer = 1 - z;
+                int pos = (1 - y) * 3;
+                pieces[layer][pos] = temp[i];
+            }
+            break;
+        }
+
+        case 'L': {
+            int index = 0;
+            for (int layer = 0; layer < 3; layer++) {
+                for (int pos = 0; pos < 9; pos += 3) {
+                    temp[index++] = pieces[layer][pos];
+                }
+            }
+
+            for (int i = 0; i < 9; i++) {
+                temp[i].rotate(rotMatricies.LCCW);
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int y = temp[i].pos[1];
+                int z = temp[i].pos[2];
+                int layer = 1 - z;
+                int pos = (1 - y) * 3;
+                pieces[layer][pos] = temp[i];
+            }
+            break;
+        }
+
+        case 'd': {
+            int index = 0;
+            for (int layer = 0; layer < 3; layer++) {
+                for (int pos = 6; pos < 9; pos++) {
+                    temp[index++] = pieces[layer][pos];
+                }
+            }
+
+            for (int i = 0; i < 9; i++) {
+                temp[i].rotate(rotMatricies.DCW);
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int x = temp[i].pos[0];
+                int z = temp[i].pos[2];
+                int layer = 1 - z;
+                int pos = 6 + (x + 1);
+                pieces[layer][pos] = temp[i];
+            }
+            break;
+        }
+
+        case 'D': {
+            int index = 0;
+            for (int layer = 0; layer < 3; layer++) {
+                for (int pos = 6; pos < 9; pos++) {
+                    temp[index++] = pieces[layer][pos];
+                }
+            }
+
+            for (int i = 0; i < 9; i++) {
+                temp[i].rotate(rotMatricies.DCCW);
+            }
+
+            for (int i = 0; i < 9; i++) {
+                int x = temp[i].pos[0];
+                int z = temp[i].pos[2];
+                int layer = 1 - z;
+                int pos = 6 + (x + 1);
+                pieces[layer][pos] = temp[i];
+            }
+            break;
+        }
+        case 'u': {// R L F2 B2 R' L' D R L F2 B2 R' L' 
+            rotationCount += 16;
+            rotateFace('r');
+            rotateFace('l');
+            rotateFace('f');
+            rotateFace('f');
+            rotateFace('b');
+            rotateFace('b');
+            rotateFace('R');
+            rotateFace('L');
+            rotateFace('d');
+            rotateFace('r');
+            rotateFace('l');
+            rotateFace('f');
+            rotateFace('f');
+            rotateFace('b');
+            rotateFace('b');
+            rotateFace('R');
+            rotateFace('L');
+            break;
+            }
+
+        case 'U': {// L R F2 B2 L' R' D' L R F2 B2 L' R'
+            rotationCount += 16;
+            rotateFace('l');
+            rotateFace('r');
+            rotateFace('f');
+            rotateFace('f');
+            rotateFace('b');
+            rotateFace('b');
+            rotateFace('L');
+            rotateFace('R');
+            rotateFace('D');
+            rotateFace('l');
+            rotateFace('r');
+            rotateFace('f');
+            rotateFace('f');
+            rotateFace('b');
+            rotateFace('b');
+            rotateFace('L');
+            rotateFace('R');
+            break;
+            }
+        }   
     }
+
+    void randomize(int numMoves) {
+        const char moves[] = {'f', 'F', 'r', 'R', 'b', 'B', 'l', 'L', 'd', 'D', 'u', 'U'};
+        
+        for (int i = 0; i < numMoves; i++) {
+            int randomIndex = rand() % 12;
+            char move = moves[randomIndex];
+            rotateFace(move);
+        }
+    }
+
+
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,20 +1,53 @@
 // main.cpp
 
+#include <string>
 #include "cube.hpp"
 
+void takeCommands();
+
 int main() {
-    virCube cube;
-
-
-    cube.printCube();
-    cube.pieces[0][3].print();
-    cube.rotateFace('f');
-    std::cout << std::endl;
-    cube.printCube();
-    cube.pieces[0][3].print();
-
-
-
-
+    takeCommands();
+    
     return 0;
+}
+
+void takeCommands() {
+    virCube cube;
+    std::string command;
+
+    cube.printCube();
+    std::cout << std::endl;
+
+    std::cout << "Enter commands (f,F,r,R,b,B,l,L,d,D,u,U), 'random', or 'q' to quit:" << std::endl;
+    
+    while (true) {
+        std::cout << "> ";
+        std::cin >> command;
+        
+        if (command == "q" || command == "Q") {
+            break;
+        }
+        
+        if (command == "random" || command == "randomize") {
+            int moves;
+            std::cout << "How many random moves? ";
+            std::cin >> moves;
+            cube.randomize(moves);  // or whatever number of moves you want
+            cube.printCube();
+            std::cout << std::endl;
+        }
+        else if (command.length() == 1 && 
+            (command[0] == 'f' || command[0] == 'F' ||
+            command[0] == 'r' || command[0] == 'R' ||
+            command[0] == 'b' || command[0] == 'B' ||
+            command[0] == 'l' || command[0] == 'L' ||
+            command[0] == 'd' || command[0] == 'D' ||
+            command[0] == 'u' || command[0] == 'U')) {
+            cube.rotateFace(command[0]);
+            cube.printCube();
+            std::cout << std::endl;
+        } else {
+            std::cout << "Invalid command. Please try again." << std::endl;
+        }
+    }
 }
